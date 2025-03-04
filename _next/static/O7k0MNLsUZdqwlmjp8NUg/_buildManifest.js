3963ceb2ce4bc6595af120cdb6a4510f049090bb
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2025-03-04 18:29:08 +0530

    first commit

---
+++ _next/static/O7k0MNLsUZdqwlmjp8NUg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/12-fe21f777957cd869.js","static/chunks/pages/index-3d620c2dc99fa0c5.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
