c1c720ad366e5165424efe0ff66dee5ac48c2804
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2025-03-05 15:33:30 +0530

    codechef and leetcode added

--- components/SVGs/Leetcode/index.js
+++ components/SVGs/Leetcode/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 
 const LeetCode = (props) => {
   return (
-    <svg
+    <img
       {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      role="img"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-leetcode"
-    >
-      <title>LeetCode</title>
-      <path d="M16.88 3.549l3.537 3.537-2.122 2.122-3.536-3.536 2.121-2.123zM11.465 2L7.05 6.414l-1.414-1.414L10.05.586l1.414 1.414zM2.343 9.172l9.9 9.9-2.122 2.122-9.9-9.9 2.122-2.122zM12.879 20.293l6.364-6.364 1.415 1.414-6.364 6.364-1.415-1.414z"></path>
-    </svg>
+      src="/Portfolio/assets/leetcode.png"
+      alt="LeetCode"
+      className="w-6 h-6"
+    />
   );
 };
 
